9b85f246a01bb7cd353d69d90c97dc7bd53bbcdb
mrgone12 <piotrek18334@gmail.com>
2020-03-06 22:39:36 +0100

    -PokemonAdress struct changed to PokemonModel -Added pokeball imagage to DetailView -Added tabs to DetailView -DetailView: now image is not in center of image as well as top edge of bottom view

---
+++ Pokedex/Views/detailSegmentedControl.swift
@@ -0,0 +1,21 @@
+//
+//  detailSegmentedControl.swift
+//  Pokedex
+//
+//  Created by Piotr on 06/03/2020.
+//  Copyright © 2020 Piotr. All rights reserved.
+//
+
+import UIKit
+
+class detailSegmentedControl: UISegmentedControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
