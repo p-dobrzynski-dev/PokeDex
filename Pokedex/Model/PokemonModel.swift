c3658c4b39953013de0ac2e67c4687fa923f805d
mrgone12 <piotrek18334@gmail.com>
2020-04-21 16:00:24 +0200

    Added Missing files

---
+++ Pokedex/Model/PokemonModel.swift
@@ -0,0 +1,37 @@
+//
+//  PokemonAdress.swift
+//  Pokedex
+//
+//  Created by Piotr on 17/02/2020.
+//  Copyright © 2020 Piotr. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct PokemonModel {
+    var urlAdress = ""
+    var pokemonName = ""
+    var pokemonID = 0
+    var pokemonImage = UIImage()
+    var pokemonTypesList = [String]()
+
+    func fullPokemonID() -> String {
+        let fullID = "#\(String(format: "%03d", pokemonID ))"
+        return fullID
+    }
+    
+    func getSortedPokemonTypesListNames(){
+        
+    }
+}
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + self.lowercased().dropFirst()
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
